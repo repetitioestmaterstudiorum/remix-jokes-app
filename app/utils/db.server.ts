f9c346062642835d29115771013bb2340e7c00e0
Thomas <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2022-06-04 21:20:50 +0200

    db connection

---
+++ app/utils/db.server.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const db: PrismaClient =
+  process.env.NODE_ENV === "production" ? new PrismaClient() : getDevDb();
+
+// unfortunately, this can't be run inside getDevDb() and will therefore also be run in production
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+function getDevDb() {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+  }
+  return global.__db;
+}
+
+export { db };
+
+// because of the `.server.ts` ending of the filename, this will not be included in the build (esbuild specific)
