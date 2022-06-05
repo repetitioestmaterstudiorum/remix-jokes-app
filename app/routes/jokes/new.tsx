ff71ad80d5bb0d1d4cd91d93267a8f84610d1092
Thomas <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2022-06-05 17:53:15 +0200

    mutation - add new data to db via form

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,31 @@
+import type { Joke } from "@prisma/client";
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  // get data from HTML form
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error(
+      `action(): form contents submitted incorrectly (name: ${name}, content: ${content})`
+    );
+  }
+
+  // insert data into the DB
+  const joke: Joke | null = await db.joke.create({
+    data: { name, content },
+  });
+  if (!joke)
+    throw new Error(
+      `action(): could not create joke with name ${name} and content ${content}`
+    );
+
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
